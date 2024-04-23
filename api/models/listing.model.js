6530f5a4e79c5c57e4630e6c640e0bbfe96ccb21
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2024-04-22 22:30:45 -0500

    list mod update

---
+++ api/models/listing.model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    }
+},
+  {
+    timestamps: true,
+  }
+);
