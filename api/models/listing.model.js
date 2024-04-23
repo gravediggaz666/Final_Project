81537591c549954a024cde0a9336a91da255b034
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2024-04-22 23:08:56 -0500

    api/mod list

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -13,9 +13,51 @@ const listingSchema = new mongoose.Schema(
     address: {
       type: String,
       required: true,
-    }
-},
-  {
-    timestamps: true,
-  }
+    },
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    discountPrice: {
+      type: Number,
+      required: true,
+    },
+    bathrooms: {
+      type: Number,
+      required: true,
+    },
+    bedrooms: {
+      type: Number,
+      required: true,
+    },
+    furnished: {
+      type: Boolean,
+      required: true,
+    },
+    parking: {
+      type: Boolean,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    offer: {
+      type: Boolean,
+      required: true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+export default Listing;
