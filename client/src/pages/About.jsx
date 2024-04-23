37a23d5a4ff895c5bb0d7d51c62ecfc830503eb6
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2024-04-23 09:17:09 -0500

    about page

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Swantage Cars</h1>
+      <p className='mb-4 text-slate-700'>
+        Welcome to our Swantage Cars enthusiast's haven! Step into a world where timeless 
+        elegance meets mechanical mastery, where each vehicle tells a story of bygone eras 
+        and open roads. Our vintage car website is your gateway to a curated collection of 
+        automotive treasures, meticulously restored and waiting to ignite your passion for 
+        classic automobiles.</p>
+      <p className='mb-4 text-slate-700'>
+       Our mission is to guide our clients on an exhilarating journey through the world of
+       vintage automobiles. With our unwavering commitment to excellence, personalized 
+       service, and profound knowledge of classic cars, we're dedicated to helping enthusiasts 
+       achieve their automotive dreams.
+       Whether you're seeking to acquire a timeless gem for your collection, sell a cherished 
+       classic to a discerning buyer, or even rent a vintage masterpiece for a special occasion, 
+       we're here to navigate the process with you. Our team of vintage car aficionados brings a 
+       wealth of expertise and a keen understanding of the market to ensure that every transaction 
+       is smooth, seamless, and satisfying.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our team of enthusiasts boasts a rich tapestry of experience and expertise in the world 
+        of classic automobiles. With a deep-rooted passion for vintage cars and a wealth of knowledge 
+        spanning generations, we are dedicated to delivering unparalleled service to our clients.</p>
+    </div>
+  )
+}
