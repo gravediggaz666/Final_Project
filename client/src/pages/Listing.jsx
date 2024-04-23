8c0fde4e340ef66c402df670292a8b297d7b7065
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2024-04-23 09:20:18 -0500

    sign up src/pages in client

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
+
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
