d9354248303922fbbf19655c6e13b7bec585b39e
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2024-04-22 22:43:07 -0500

    api.cont auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,3 +2,13 @@ import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
+
+export const createListing = async (req, res, next) => {
+    try {
+      const listing = await Listing.create(req.body);
+      return res.status(201).json(listing);
+    } catch (error) {
+      next(error);
+    }
+  };
+  
