49c79ac64e2bba82e5598db87b39821ab41ac2ba
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2024-04-24 13:35:33 -0500

    updated home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,19 +7,76 @@ import 'swiper/css/bundle';
 import ListingItem from '../components/ListingItem';
 
 export default function Home() {
-  const [offerListings, setOfferListings] = useState([]);
-  const [saleListings, setSaleListings] = useState([]);
-  const [rentListings, setRentListings] = useState([]);
+  const [carListings, setCarListings] = useState([]);
   SwiperCore.use([Navigation]);
-  console.log(offerListings);
+
   useEffect(() => {
-    const fetchOfferListings = async () => {
+    const fetchCarListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?offer=true&limit=4');
+        const res = await fetch('/api/car/get?limit=4');
         const data = await res.json();
-        setOfferListings(data);
-        fetchRentListings();
+        setCarListings(data);
       } catch (error) {
         console.log(error);
       }
     };
+
+    fetchCarListings();
+  }, []);
+
+  return (
+    <div>
+      {/* top */}
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
+          Find your next <span className='text-slate-500'>dream car</span>
+          <br />
+          with ease
+        </h1>
+        <div className='text-gray-400 text-xs sm:text-sm'>
+          Browse through our collection of cars to find your perfect match.
+        </div>
+        <Link
+          to={'/search'}
+          className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
+        >
+          Let's get started...
+        </Link>
+      </div>
+
+      {/* swiper */}
+      <Swiper navigation>
+        {carListings &&
+          carListings.length > 0 &&
+          carListings.map((listing) => (
+            <SwiperSlide key={listing._id}>
+              <div
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+                className='h-[500px]'
+              ></div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      {/* listing results for cars */}
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+        {carListings && carListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>Recent Car Listings</h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search'}>Show more cars</Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {carListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
