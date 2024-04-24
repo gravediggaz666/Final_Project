0194ecd809896a7bc590c3385b5afc7d9cd87d2f
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2024-04-24 17:19:52 -0500

    fixed for user creation

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,6 +2,14 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
     username: {
       type: String,
       required: true,
@@ -16,10 +24,18 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    avatar:{
+    avatar: {
       type: String,
       default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     },
+    location: {
+      type: String,
+      required: true,
+    },
+    favoriteListings: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Listing',
+    }],
   },
   { timestamps: true }
 );
