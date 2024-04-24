7f170b48d90bb3db7770e32af297f3c8e32335f9
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2024-04-23 22:09:57 -0500

    for the listing of cars

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { FaSearch } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+
+export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
