0278bc133230b6139ff705e363dcd7252529770c
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2024-04-24 20:06:11 -0500

    removed unwanted extras

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,14 +2,6 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
     username: {
       type: String,
       required: true,
@@ -28,14 +20,6 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     },
-    location: {
-      type: String,
-      required: true,
-    },
-    favoriteListings: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Listing',
-    }],
   },
   { timestamps: true }
 );
