29510165c51d7fb86778bf964caa3e58c0ec4ac2
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2024-04-24 22:57:11 -0500

    fixed listingitem and what the car has

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -1,40 +1,48 @@
 import { Link } from 'react-router-dom';
-import { FaGasPump, FaCar, FaCalendarAlt } from 'react-icons/fa';
+import { MdLocationOn } from 'react-icons/md';
 
-export default function CarItem({ car }) {
+export default function ListingItem({ listing }) {
   return (
     <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
-      <Link to={`/car/${car._id}`}>
+      <Link to={`/listing/${listing._id}`}>
         <img
           src={
-            car.imageUrls[0] ||
+            listing.imageUrls[0] ||
             'https://bringatrailer.com/wp-content/uploads/2020/02/1992_chevrolet_c1500_158324397444d6279D1E0BFE4-7EB1-4B15-93BC-CB61E7842546.jpeg' 
           }
-          alt='car cover'
+          alt='listing cover'
           className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
         />
         <div className='p-3 flex flex-col gap-2 w-full'>
           <p className='truncate text-lg font-semibold text-slate-700'>
-            {car.make} {car.model}
+            {listing.name}
           </p>
           <div className='flex items-center gap-1'>
-            <FaCar className='h-4 w-4 text-green-700' />
+            <MdLocationOn className='h-4 w-4 text-green-700' />
             <p className='text-sm text-gray-600 truncate w-full'>
-              {car.year}
+              {listing.model}
             </p>
           </div>
           <p className='text-sm text-gray-600 line-clamp-2'>
-            {car.description}
+            {listing.description}
           </p>
           <p className='text-slate-500 mt-2 font-semibold '>
-            ${car.price.toLocaleString('en-US')}
+            $
+            {listing.offer
+              ? listing.discountPrice.toLocaleString('en-US')
+              : listing.regularPrice.toLocaleString('en-US')}
+            {listing.type === 'rent' && ' / month'}
           </p>
           <div className='text-slate-700 flex gap-4'>
             <div className='font-bold text-xs'>
-              {car.engine}
+              {listing.doors > 2
+                ? `${listing.doors} doors `
+                : `${listing.doors} door `}
             </div>
             <div className='font-bold text-xs'>
-              {car.transmission}
+              {listing.windows > 2
+                ? `${listing.windows} windows `
+                : `${listing.windows} window `}
             </div>
           </div>
         </div>
